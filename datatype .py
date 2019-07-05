84b6b6e7e5ea74608701bf19c5b797b32ca799d6
nooknic <52314807+nooknic@users.noreply.github.com>
2019-07-05 16:31:58 +0700

    Add files via upload

---
+++ datatype .py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Wellcome to NooknicShop ")
+print(".....................................  ")
+print("Water   1   10   THB         ")
+print("Lay       2   40    THB        ")
+print("banana 1 10   THB         ")
+
+
+# In[6]:
+
+
+print("Wellcome to NooknicShop ")
+print("..........................................")
+print("Water", 1 , 10 , "THB",1*10, "THB",       )
+print("Lay"   ,  2 , 40 , "THB" ,2*20,"THB",     )
+print("banana",1 , 10, "THB" ,1*10, "THB",     )
+print("Total", 10+40+10  ,         "THB"     )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
