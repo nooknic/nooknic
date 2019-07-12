6e5d3272507c95f9943d5bc0b02469a1b7cb31f5
nooknic <52314807+nooknic@users.noreply.github.com>
2019-07-12 14:17:54 +0700

    Add files via upload

---
+++ Subtest1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("#1.น.ส.นีรชา ชุมมิน 362515241007 EE36241N")
+print("#2.น.ส.กวินนา ชุมมิน 362515241001 EE36241N")
+
+
+# In[3]:
+
+
+print("R1=100 Ohm.")
+print("R2=300 Ohm.")
+print("RT=(R1*R2)/(R1+R2)")
+print("RT=75 Ohm")
+
+
+# In[ ]:
+
+
+
+
