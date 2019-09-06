84d27d752c7f495d96893f5e5e43731548747e2e
nooknic <52314807+nooknic@users.noreply.github.com>
2019-09-06 23:04:09 +0700

    Add files via upload

---
+++ Triangle3 (1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# นางสาวนีรชา  ชุมมิน 362515241007 EE36241N
+max = 0
+i = 3
+sum = 0
+for i in range(i):
+    if angle > max:
+        max = angle
+    angle = int(input("enter angle :"))
+    sum = sum + angle
+print("sum :", sum)
+print("hypotenuse", max)
+
